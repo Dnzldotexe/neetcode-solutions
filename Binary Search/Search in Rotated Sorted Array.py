133e003ed503918bc40093e42647d7657112327e
Danziel Cempron <104302220+Dnzldotexe@users.noreply.github.com>
2026-05-06 13:52:22 +0800

    refactor: repeated code

--- Binary Search/Search in Rotated Sorted Array.py
+++ Binary Search/Search in Rotated Sorted Array.py
@@ -1,39 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
-        mid = left + ((right - left) // 2)
+        min_num = self.findMin(nums)
 
-        if nums[left] <= nums[mid] <= nums[right]:
-            left, right = 0, len(nums) - 1
-
-            while left <= right:
-                mid = left + ((right - left) // 2)
-
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] < target:
-                    left = mid + 1 # go right
-                else:
-                    right = mid - 1 # go left
-
-            return -1
-
+        if nums[left] == nums[min_num]:
+            mid = left + ((right - left) // 2)
         else:
-            left, right = 0, len(nums) - 1
-            mid = self.findMin(nums)
+            mid = min_num
 
-            while left <= right:
-                if nums[mid] == target:
-                    return mid
-                if nums[mid] <= target <= nums[right]:
-                    left = mid + 1 # go right
-                    print("right")
-                else:
-                    right = mid - 1 # go left
-                    print("left")
-                mid = left + ((right - left) // 2)
+        while left <= right:
+            if nums[mid] == target:
+                return mid
+            if nums[mid] <= target <= nums[right]:
+                left = mid + 1 # go right
+                print("right")
+            else:
+                right = mid - 1 # go left
+                print("left")
+            mid = left + ((right - left) // 2)
 
-            return -1
+        return -1
 
     
     def findMin(self, nums: List[int]) -> int:
